8dbce153bfcbbfa0cb8aef03c7ce45f511b10e53
TA <appulegugure@gmail.com>
2019-07-04 21:55:58 +0900

    Initial Commit

---
+++ Github_API/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Github_API
+//
+//  Created by アップルググれ on 2019/07/04.
+//  Copyright © 2019 アップルググ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
